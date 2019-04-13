c21c1d172e8d19d900fb9adc2f516ad85932b656
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-13 22:07:45 +0200

    Added docblock to macro

--- lib/toothpick/macros.ex
+++ lib/toothpick/macros.ex
@@ -1,6 +1,12 @@
 defmodule Toothpick.Macros do
   use Toothpick.Constants
 
+  @doc """
+  Creates functions tokens() for each symbol in symbols argument
+
+  name (atom) - name to be used for symbol identification
+  symbols (atom) - symbols to be retrieved from the Toothpick.Constants module
+  """
   defmacro tokens(name, symbols) do
     Enum.map(get(symbols), fn symbol -> create(name, symbol) end)
   end
