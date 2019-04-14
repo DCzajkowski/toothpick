35594a4bdf4531ad44cf6eb07347f61859ca34ef
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-14 21:20:21 +0200

    Added new test for parsing function with arguments

--- test/toothpick_parser_test.exs
+++ test/toothpick_parser_test.exs
@@ -29,5 +29,36 @@ defmodule ToothpickParserTest do
       ]},
     ]
   end
+
+  test "correctly parses function with arguments" do
+    assert parse([
+      {:keyword, "fun"},
+      {:identifier, "add"},
+      {:variable, "a"},
+      {:variable, "b"},
+      {:new_line, "\n"},
+      {:keyword, "return"},
+      {:variable, "a"},
+      {:new_line, "\n"},
+      {:punctuator, "."},
+      {:new_line, "\n"},
+    ]) == [
+      {:function_declaration, [
+        {:keyword, "fun"},
+        {:identifier, "main"},
+        {:arguments, [
+          {:variable, "a"},
+          {:variable, "b"},
+        ]},
+        {:function_body, [
+          {:return_statement, [
+            {:keyword, "return"},
+            {:expression, [
+              {:string, "Hello, World!"},
+            ]},
+          ]},
+        ]},
+      ]},
+    ]
   end
 end
