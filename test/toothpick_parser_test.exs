f55c31d71ee431f220688f54365f1eb9a2e4b040
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-15 01:27:09 +0200

    Format lists

--- test/toothpick_parser_test.exs
+++ test/toothpick_parser_test.exs
@@ -5,50 +5,54 @@ defmodule ToothpickParserTest do
   doctest Toothpick.Parser
 
   test "correctly parses function without arguments" do
-    assert parse(
-      keyword: "fun",
-      identifier: "main",
-      new_line: "\n",
-      keyword: "return",
-      string: "Hello, World!",
-      new_line: "\n",
-      punctuator: ".",
-      new_line: "\n"
-    ) == [
-      function_declaration: [
+    assert(
+      parse(
         keyword: "fun",
         identifier: "main",
-        function_body: [
-          return_statement: [
-            keyword: "return",
-            expression: [string: "Hello, World!"]
+        new_line: "\n",
+        keyword: "return",
+        string: "Hello, World!",
+        new_line: "\n",
+        punctuator: ".",
+        new_line: "\n"
+      ) == [
+        function_declaration: [
+          keyword: "fun",
+          identifier: "main",
+          function_body: [
+            return_statement: [
+              keyword: "return",
+              expression: [string: "Hello, World!"]
+            ]
           ]
         ]
       ]
-    ]
+    )
   end
 
   test "correctly parses function with arguments" do
-    assert parse(
-      keyword: "fun",
-      identifier: "add",
-      variable: "a",
-      variable: "b",
-      new_line: "\n",
-      keyword: "return",
-      variable: "a",
-      new_line: "\n",
-      punctuator: ".",
-      new_line: "\n"
-    ) == [
-      function_declaration: [
+    assert(
+      parse(
         keyword: "fun",
         identifier: "add",
-        function_arguments: [variable: "a", variable: "b"],
-        function_body: [
-          return_statement: [keyword: "return", expression: [variable: "a"]]
+        variable: "a",
+        variable: "b",
+        new_line: "\n",
+        keyword: "return",
+        variable: "a",
+        new_line: "\n",
+        punctuator: ".",
+        new_line: "\n"
+      ) == [
+        function_declaration: [
+          keyword: "fun",
+          identifier: "add",
+          function_arguments: [variable: "a", variable: "b"],
+          function_body: [
+            return_statement: [keyword: "return", expression: [variable: "a"]]
+          ]
         ]
       ]
-    ]
+    )
   end
 end
