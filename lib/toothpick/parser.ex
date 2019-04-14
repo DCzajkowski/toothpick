f579304a62576e4668d33b77cde40fae2c0f253f
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-14 21:19:25 +0200

    Removed dead code comment

--- lib/toothpick/parser.ex
+++ lib/toothpick/parser.ex
@@ -45,31 +45,4 @@ defmodule Toothpick.Parser do
     subtree = {:expresion, children}
     {tree ++ [subtree], tail}
   end
-
-  # def parser(tokens) do
-  #   %{
-  #     type: "FunctionDeclaration",
-  #     id: %{
-  #       type: "Identifier",
-  #       name: "main"
-  #     },
-  #     params: [],
-  #     body: %{
-  #       type: "BlockStatement",
-  #       body: [
-  #         %{
-  #           type: "ReturnStatement",
-  #           argument: %{
-  #             type: "Literal",
-  #             value: "Hello, World!",
-  #             raw: "'Hello, World!'"
-  #           }
-  #         }
-  #       ]
-  #     },
-  #     generator: false,
-  #     expression: false,
-  #     async: false
-  #   }
-  # end
 end
