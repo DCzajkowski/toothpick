d67f10045068889e236d91f3bf7d6df9802fd686
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-16 01:21:11 +0200

    Improved styling and added comments

--- lib/toothpick/parser.ex
+++ lib/toothpick/parser.ex
@@ -1,67 +1,65 @@
 defmodule Toothpick.Parser do
   def parse(tokens), do: program(tokens)
 
-  def program(tokens) do
-    {children, _} = function([], tokens)
-    children
-  end
+  # Parses the very top level of a program. A program can consist only of function declarations.
+  defp program(tokens), do: elem(function([], tokens), 0)
 
-  def function(tree, [{:keyword, "fun"}, {:identifier, name} | tail]) do
+  # Parses the function definition. A function always starts with a keyword 'fun' and identifier.
+  # It is then followed by an optional list of arguments and body.
+  defp function(tree, [{:keyword, "fun"}, {:identifier, name} | tail]) do
     children = [{:keyword, "fun"}, {:identifier, name}]
+
     {children, tail} = arguments(children, tail)
     {children, tail} = body(children, tail)
-    subtree = {:function_declaration, children}
-    function(tree ++ [subtree], tail)
+
+    function(tree ++ [{:function_declaration, children}], tail)
   end
 
-  def function(tree, tail), do: {tree, tail}
+  defp function(tree, tail), do: {tree, tail}
 
-  def arguments(tree, [{:variable, name} | tail]) do
+  # Arguments of a function is a list of variables. It ends, if any other token than a variable
+  # is encountered (normally a {:new_line, _} or {:punctuator, '->'}).
+  defp arguments(tree, [{:variable, name} | tail]) do
     children = [{:variable, name}]
+
     {children, tail} = accumulate_args(children, tail)
-    subtree = {:function_arguments, children}
-    {tree ++ [subtree], tail}
+
+    {tree ++ [{:function_arguments, children}], tail}
   end
 
-  def arguments(tree, tail), do: {tree, tail}
+  defp arguments(tree, tail), do: {tree, tail}
+  defp accumulate_args(args, [{:variable, name} | tail]), do: accumulate_args(args ++ [{:variable, name}], tail)
+  defp accumulate_args(args, tail), do: {args, tail}
 
-  def accumulate_args(args, [{:variable, name} | tail]) do
-    args = args ++ [{:variable, name}]
-    accumulate_args(args, tail)
+  # Function's body always starts with a {:new_line, _} and is terminated by a {:punctuator, '.'}.
+  defp body(tree, [{:new_line, _} | tail]) do
+    children = []
+
+    {children, tail} = statement(children, tail)
+
+    {tree ++ [{:function_body, children}], tail}
   end
 
-  def accumulate_args(args, tail), do: {args, tail}
+  defp body(tree, [{:punctuator, "."} | tail]), do: {tree ++ [{:function_body, []}], tail}
 
-  def body(tree, [{:new_line, _} | tail]) do
-    {children, tail} = statement([], tail)
-    subtree = {:function_body, children}
-    {tree ++ [subtree], tail}
-  end
-
-  def body(tree, [{:punctuator, "."} | tail]) do
-    subtree = {:function_body, []}
-    {tree ++ [subtree], tail}
-  end
-
-  def statement(tree, [{:keyword, "return"} | tail]) do
+  # There are two types of statements. :return_statement and :if_statement. In both cases,
+  # they are terminated by a {:punctuator, '.'}.
+  defp statement(tree, [{:keyword, "return"} | tail]) do
     children = [{:keyword, "return"}]
+
     {children, tail} = expression(children, tail)
-    subtree = {:return_statement, children}
-    statement(tree ++ [subtree], tail)
+
+    statement(tree ++ [{:return_statement, children}], tail)
   end
 
-  def statement(tree, [{:new_line, _} | tail]), do: statement(tree, tail)
-  def statement(tree, [{:punctuator, "."} | tail]), do: {tree, tail}
 
-  def expression(tree, [{:string, value} | tail]) do
-    children = [{:string, value}]
-    subtree = {:expression, children}
-    {tree ++ [subtree], tail}
-  end
 
-  def expression(tree, [{:variable, value} | tail]) do
-    children = [{:variable, value}]
-    subtree = {:expression, children}
-    {tree ++ [subtree], tail}
-  end
+
+  defp statement(tree, [{:new_line, _} | tail]), do: statement(tree, tail)
+  defp statement(tree, [{:punctuator, "."} | tail]), do: {tree, tail}
+
+  # An expression is a simplest, smallest chunk of the code. It is normally a :string,
+  # :variable or :integer.
+  defp expression(tree, [{:string, value} | tail]), do: {tree ++ [{:expression, [{:string, value}]}], tail}
+  defp expression(tree, [{:variable, value} | tail]), do: {tree ++ [{:expression, [{:variable, value}]}], tail}
 end
