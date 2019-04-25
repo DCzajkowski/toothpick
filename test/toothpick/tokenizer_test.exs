6160c808c792d1f5ac4a53d3fa5e87edaf300223
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-26 00:51:38 +0200

    Updated function without arguments to reflect current syntax

--- test/toothpick/tokenizer_test.exs
+++ test/toothpick/tokenizer_test.exs
@@ -24,6 +24,7 @@ defmodule TokenizerTest do
       tokens(stub("function_without_arguments")) == [
         keyword: "fun",
         identifier: "main",
+        punctuator: "->",
         new_line: "\n",
         keyword: "return",
         string: "Hello, World!",
