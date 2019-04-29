d649d2a060d23415ad91064b128cb5cbfdbc1ea9
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-29 20:05:34 +0200

    Prepared the tokenizer for if's

--- lib/toothpick/constants.ex
+++ lib/toothpick/constants.ex
@@ -1,7 +1,7 @@
 defmodule Toothpick.Constants do
   defmacro __using__(_options) do
     quote do
-      def get(:punctuators), do: ["->", "(", ")", ".", ",", "[", "]", ">", "$"]
+      def get(:punctuators), do: ["->", "(", ")", ".", ",", "[", "]", ">", "$", ":"]
       def get(:new_line_chars), do: ["\n", "\r\n"]
       def get(:keywords), do: ["fun", "return", "if", "elif", "else"]
       def get(:booleans), do: ["true", "false"]
